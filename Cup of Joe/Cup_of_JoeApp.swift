82b2a589f01a6c14cee553e1bfa002c1b1be810b
Joseph Samonte <josephisnt@hotmail.com>
2023-04-18 20:22:40 -0500

    Initial Commit

---
+++ Cup of Joe/Cup_of_JoeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cup_of_JoeApp.swift
+//  Cup of Joe
+//
+//  Created by Joseph Samonte on 4/18/23.
+//
+
+import SwiftUI
+
+@main
+struct Cup_of_JoeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
